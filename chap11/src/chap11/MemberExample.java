ed1011cbc884e4701d58422f0d5ae496eea10d16
jihoBae <jiho@dgu.ac.kr>
2020-12-29 20:24:43 +0900

    study object class equals method & hashcode.

---
+++ chap11/src/chap11/MemberExample.java
@@ -0,0 +1,24 @@
+package chap11;
+
+public class MemberExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Member obj1 = new Member("blue");
+		Member obj2 = new Member("blue");
+		Member obj3 = new Member("red");
+		
+		if(obj1.equals(obj2)) {
+			System.out.println("obj1 == obj2");
+		} else {
+			System.out.println("obj1 != obj2");
+		}
+	
+		if(obj1.equals(obj3)) {
+			System.out.println("obj1 == obj3");
+		} else {
+			System.out.println("obj1 != obj3");
+		}
+	}
+
+}
